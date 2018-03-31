5b3437cc65ac851b84d4f06784e07c0d494567eb
Mitch Gavan <mitchgavan@gmail.com>
2018-04-01 08:23:18 +1000

    score a good move

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -25,7 +25,7 @@ defmodule GameTest do
     assert game.game_state != :already_used
   end
 
-    test "second occurrence of letter is not already used" do
+  test "second occurrence of letter is not already used" do
     game = Game.new_game()
 
     { game, _tally } = Game.make_move(game, "x")
@@ -33,6 +33,32 @@ defmodule GameTest do
 
     { game, _tally } = Game.make_move(game, "x")
     assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognised" do
+    game = Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    game = Game.new_game("wibble")
+    { game, _tally } = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    { game, _tally } = Game.make_move(game, "i")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    { game, _tally } = Game.make_move(game, "b")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    { game, _tally } = Game.make_move(game, "l")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    { game, _tally } = Game.make_move(game, "e")
+    assert game.game_state == :won
+    assert game.turns_left == 7
   end
 
 end
