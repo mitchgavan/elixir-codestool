199052618eb35bd37615751252615bcb9697f688
Mitch Gavan <mitchgavan@gmail.com>
2018-05-27 10:23:57 +1000

    add svg graphic

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -3,6 +3,14 @@ defmodule GallowsWeb.HangmanView do
 
   import Gallows.Views.Helpers.GameStateHelper
 
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
+
   def game_over?(%{ game_state: game_state }) do
     game_state in [ :won, :lost ]
   end
