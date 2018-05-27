3315a463407aa86b5643576f9439853d8d3942b2
Mitch Gavan <mitchgavan@gmail.com>
2018-05-27 17:00:47 +1000

    push the tally from the server

--- socket_gallows/assets/js/hangman_socket.js
+++ socket_gallows/assets/js/hangman_socket.js
@@ -8,15 +8,27 @@ export default class HangmanSocket {
     }
 
     connect_to_hangman() {
+        this.setup_channel()
+        this.channel.on('tally', tally => {
+            console.dir(tally)
+        })
+    }
+
+    setup_channel() {
         this.channel = this.socket.channel('hangman:game', {})
         this.channel
             .join()
             .receive('ok', res => {
-                console.log('connected ' + res)
+                console.log('connected: ' + res)
+                this.fetch_tally()
             })
             .receive('error', res => {
                 alert(res)
                 throw(res)
             })
+    }
+
+    fetch_tally() {
+        this.channel.push('tally', {})
     }
 }
--- socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
+++ socket_gallows/lib/socket_gallows_web/channels/hangman_channel.ex
@@ -1,8 +1,24 @@
 defmodule SocketGallows.Web.HangmanChannel do
 
-   use Phoenix.Channel
+    require Logger
 
-   def join("hangman:game", _, socket) do
-    { :ok, socket }
-   end 
+    use Phoenix.Channel
+
+    def join("hangman:game", _, socket) do
+        game = Hangman.new_game()
+        socket = assign(socket, :game, game)
+        { :ok, socket }
+    end 
+
+    def handle_in("tally", _, socket) do
+        game = socket.assigns.game
+        tally = Hangman.tally(game)
+        push(socket, "tally", tally)
+        { :noreply, socket }
+    end
+
+    def handle_in(message, _, socket) do
+        Logger.error("whoops, we don't serve this message")
+    end
+   
 end
